c7c8a0c42a99745f399dbe58aeb6bd5c821d5428
ShumontoPradhan <157868046+ShumontoPradhan@users.noreply.github.com>
2025-05-19 14:56:12 +0530

    Time: 12 ms (5.11%), Space: 45.4 MB (19.91%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(n);
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < n; i++){
+            for(int neighbor : graph[i]){
+                adj.get(i).add(neighbor);  //undirected
+                adj.get(neighbor).add(i);
+            }
+        }
+        
+        int[] colors  = new int[n]; //(red - 1) , (yellow - 0)
+        Arrays.fill(colors, -1); //this will also work as visited array 
+        for(int i = 0; i < n; i++){
+            if(colors[i] == -1){
+                if(!bipartite_check(adj, colors, i, 1)){  //currColour 
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean bipartite_check(ArrayList<ArrayList<Integer>> adj, int[] color,int start, int currColor){
+        color[start] = currColor;
+        for(int neighbor : adj.get(start)){
+            if(color[neighbor] == color[start]) return false;
+            if(color[neighbor] == -1){
+                if(bipartite_check(adj, color, neighbor, 1- currColor) == false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
